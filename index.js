cde20273b33c540a31a8374c53bfa8f91f3ba821
Shuchen <shuchen@vip.qq.com>
2017-03-16 17:29:59 +0800

    shuchen

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
-/**
- * Created by Administrator on 2017/3/1.
- */
-//Í¼Æ¬¼òµ¥ÇÐ»»µ÷ÓÃÓï¾ä imgtransition({speed: 3000, animate: 1000});
-$.fn.imgtransition = function(o){
-    var defaults = {
-        speed : 5000,
-        animate : 1000
-    };
-    o = $.extend(defaults, o);
-    return this.each(function(){
-        var arr_e = $("li", this);
-        arr_e.css({position: "absolute"});
-        arr_e.parent().css({margin: "0", padding: "0", "list-style": "none", overflow: "hidden"});
-        function shownext(){
-            var active = arr_e.filter(".active").length ? arr_e.filter(".active") : arr_e.first();
-            var next =  active.next().length ? active.next() : arr_e.first();
-            active.css({"z-index": 9});
-            next.css({opacity: 0.0, "z-index": 10}).addClass('active').animate({opacity: 1.0}, o.animate, function(){
-                active.removeClass('active').css({"z-index": 8});
-            });
-        }
-        arr_e.first().css({"z-index": 9});
-        setInterval(function(){
-            shownext();
-        },o.speed);
 
-    });
-};
 
 $(document).ready(function(){
-
-    $('.flash').imgtransition({
-        speed:3000,  //Í¼Æ¬ÇÐ»»Ê±¼ä
-        animate:1000 //Í¼Æ¬ÇÐ»»¹ý¶ÉÊ±¼ä
-    });
+    $('.ck-slide').ckSlide();
     $(".button").click(function(){
         $('.layer').toggle(400);
 
     })
 
-
+    var target_top = $("#footerup").offset().top;
+    $(".dropdown").on("click",function(){
+        $("html,body").animate({scrollTop:target_top},500)
+    })
 });
+
